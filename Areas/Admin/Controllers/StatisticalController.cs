8505a3a99982d4709b4909b56d53475d8037a18f
ngocdtm <ngocdtm@gmail.com>
2024-10-15 01:58:28 +0700

    Fix structure

--- Areas/Admin/Controllers/StatisticalController.cs
+++ Areas/Admin/Controllers/StatisticalController.cs
@@ -15,12 +15,11 @@ namespace WebsiteBanDoAnVaThucUong.Areas.Admin.Controllers
         // GET: Admin/Statistical
         public ActionResult Index()
         {
-            ViewBag.Stores = db.Stores.ToList();
             return View();
         }
 
         [HttpGet]
-        public ActionResult GetStatistical(string fromDate, string toDate, int? storeId)
+        public ActionResult GetStatistical(string fromDate, string toDate)
         {
             var query = from o in db.Orders
                         join od in db.OrderDetails on o.Id equals od.OrderId
@@ -32,8 +31,7 @@ namespace WebsiteBanDoAnVaThucUong.Areas.Admin.Controllers
                             Price = od.UnitPrice,
                             OriginalPrice = p.OriginalPrice,
                             ProductId = p.Id,
-                            ProductName = p.Title,
-                            StoreId = o.StoreId
+                            ProductName = p.Title
                         };
 
             if (!string.IsNullOrEmpty(fromDate))
@@ -47,46 +45,30 @@ namespace WebsiteBanDoAnVaThucUong.Areas.Admin.Controllers
                 query = query.Where(x => x.CreatedDate < endDate);
             }
 
-            if (storeId.HasValue)
+            var revenueResult = query.GroupBy(x => DbFunctions.TruncateTime(x.CreatedDate)).Select(x => new
             {
-                query = query.Where(x => x.StoreId == storeId.Value);
-            }
+                Date = x.Key.Value,
+                TotalBuy = x.Sum(y => y.Quantity * y.OriginalPrice),
+                TotalSell = x.Sum(y => y.Quantity * y.Price),
+            }).Select(x => new
+            {
+                Date = x.Date,
+                DoanhThu = x.TotalSell,
+                LoiNhuan = x.TotalSell - x.TotalBuy
+            }).ToList();
 
-            var revenueResult = query.GroupBy(x => new { Date = DbFunctions.TruncateTime(x.CreatedDate), x.StoreId })
-                .Select(x => new
-                {
-                    Date = x.Key.Date.Value,
-                    StoreId = x.Key.StoreId,
-                    TotalBuy = x.Sum(y => y.Quantity * y.OriginalPrice),
-                    TotalSell = x.Sum(y => y.Quantity * y.Price),
-                })
-                .Select(x => new
-                {
-                    Date = x.Date,
-                    StoreId = x.StoreId,
-                    DoanhThu = x.TotalSell,
-                    LoiNhuan = x.TotalSell - x.TotalBuy
-                }).ToList();
+            var productOrderResult = query.GroupBy(x => DbFunctions.TruncateTime(x.CreatedDate)).Select(x => new
+            {
+                Date = x.Key.Value,
+                TotalQuantity = x.Sum(y => y.Quantity),
+                OrderCount = x.Select(y => y.Quantity).Count()
+            }).ToList();
 
-            var productOrderResult = query.GroupBy(x => new { Date = DbFunctions.TruncateTime(x.CreatedDate), x.StoreId })
-                .Select(x => new
-                {
-                    Date = x.Key.Date.Value,
-                    StoreId = x.Key.StoreId,
-                    TotalQuantity = x.Sum(y => y.Quantity),
-                    OrderCount = x.Select(y => y.Quantity).Count()
-                }).ToList();
-
-            var topProducts = query.GroupBy(x => new { x.ProductId, x.ProductName, x.StoreId })
-                .Select(x => new
-                {
-                    ProductName = x.Key.ProductName,
-                    StoreId = x.Key.StoreId,
-                    TotalQuantity = x.Sum(y => y.Quantity)
-                })
-                .OrderByDescending(x => x.TotalQuantity)
-                .Take(5)
-                .ToList();
+            var topProducts = query.GroupBy(x => new { x.ProductId, x.ProductName }).Select(x => new
+            {
+                ProductName = x.Key.ProductName,
+                TotalQuantity = x.Sum(y => y.Quantity)
+            }).OrderByDescending(x => x.TotalQuantity).Take(5).ToList();
 
             return Json(new { RevenueData = revenueResult, ProductOrderData = productOrderResult, TopProducts = topProducts }, JsonRequestBehavior.AllowGet);
         }
@@ -133,4 +115,3 @@ namespace WebsiteBanDoAnVaThucUong.Areas.Admin.Controllers
 
     }
 }
-
