06f7dbd2cfaec182fea9233a3ba285df16093862
ngocdtm <ngocdtm@gmail.com>
2024-10-20 09:58:22 +0700

    Product menu and select branch

--- App_Start/FilterConfig.cs
+++ App_Start/FilterConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using WebsiteBanDoAnVaThucUong.Filters;
 
 namespace WebsiteBanDoAnVaThucUong
 {
@@ -8,6 +9,7 @@ namespace WebsiteBanDoAnVaThucUong
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new StoreSelectorFilter()); 
         }
     }
 }
